092cba97070f42be7ff823cf8a8ea259df5c86b5
Your name <965545669@qq.com>
2019-11-13 15:23:43 +0800

    你好

---
+++ HelloProjet/mian/java/User.java
@@ -0,0 +1,12 @@
+/**
+ * @ Author     ：mmzs.
+ * @ Date       ：Created in 15:21 2019/11/13
+ * @ Description：
+ * @ Modified By：
+ * @Version: $
+ */
+public class User {
+    public void fun(){
+        System.out.println("你好");
+    }
+}
